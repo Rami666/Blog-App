ee2fd50bb80856114de26a4817dcf72436dde09b
Rami B <ramibasheer9@gmail.com>
2023-02-17 00:20:17 +0100

    layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,20 @@
 import "./App.css";
 import Post from "./Post";
-import Header from "./Header";
+
+import { Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
+import IndexPage from "./pages/IndexPage";
 
 function App() {
   return (
-    <main>
-      <Header />
-      
-  
-      <Post />
-      <Post />
-      <Post />
-     
+    <Routes>
+      <Route path={"/"} element={<Layout />}>
 
-    </main>
+      <Route index element={<IndexPage />} />
+      <Route path={"/login"} element={<div>login page</div>} />
+
+      </Route>
+    </Routes>
   );
 }
 
